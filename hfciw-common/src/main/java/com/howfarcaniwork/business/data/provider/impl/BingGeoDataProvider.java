88d38fb514bc215e1cb8f6871765f074601bf03c
Terence Tardieux <terencetardieux@gmail.com>
2014-01-27 22:27:15 +0100

    Ajout des impl 	new file:   hfciw-common/src/main/java/com/howfarcaniwork/business/data/provider/impl/BingGeoDataProvider.java 	new file:   hfciw-common/src/main/java/com/howfarcaniwork/business/data/provider/impl/BingGeoTimeProvider.java 	new file:   hfciw-common/src/main/java/com/howfarcaniwork/business/data/provider/impl/GmapsGeoDataProvider.java 	new file:   hfciw-common/src/main/java/com/howfarcaniwork/business/data/provider/impl/GmapsGeoTimeProvider.java 	new file:   hfciw-common/src/main/java/com/howfarcaniwork/business/data/provider/impl/OpenStreetMapGeoDataProvider.java 	new file:   hfciw-common/src/main/java/com/howfarcaniwork/business/data/provider/impl/OpenStreetMapGeoTimeProvider.java

---
+++ hfciw-common/src/main/java/com/howfarcaniwork/business/data/provider/impl/BingGeoDataProvider.java
@@ -0,0 +1,26 @@
+package com.howfarcaniwork.business.data.provider.impl;
+
+import com.howfarcaniwork.business.data.geopoint.GeoPoint;
+import com.howfarcaniwork.business.data.provider.GeoDataProvider;
+
+public class BingGeoDataProvider implements GeoDataProvider {
+
+	@Override
+	public GeoPoint getUpperLeftBound(GeoPoint p, float distance) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public GeoPoint getPositionFromAddress(String address) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getAddressFromPosition(GeoPoint p, int precision) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ hfciw-common/src/main/java/com/howfarcaniwork/business/data/provider/impl/GmapsGeoTimeProvider.java
@@ -0,0 +1,30 @@
+package com.howfarcaniwork.business.data.provider.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import com.howfarcaniwork.business.data.TravelMode;
+import com.howfarcaniwork.business.data.geopoint.GeoPoint;
+import com.howfarcaniwork.business.data.provider.GeoTimeProvider;
+
+public class GmapsGeoTimeProvider implements GeoTimeProvider {
+
+	@Override
+	public long getTravelTime(GeoPoint departure, GeoPoint arrival, TravelMode m) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getMaxListLength() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Map<GeoPoint, Long> getMatrixTime(List<GeoPoint> points, TravelMode m) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
